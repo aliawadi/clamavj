294d3fe0348a59a99701dab7e58a42d682f01fe7
philvarner <philvarner@b7d57b12-2688-11df-b172-d5673a86adf5>
2010-03-03 07:25:17 +0000

    better error handling for files exceeding max size

--- trunk/src/main/java/com/philvarner/clamavj/ClamScan.java
+++ trunk/src/main/java/com/philvarner/clamavj/ClamScan.java
@@ -16,6 +16,29 @@ public class ClamScan {
 
     private static final int DEFAULT_CHUNK_SIZE = 2048;
     private static final byte[] INSTREAM = "zINSTREAM\0".getBytes();
+    private static final byte[] PING = "zPING\0".getBytes();
+    private static final byte[] STATS = "zSTATS\0".getBytes();
+    //  IDSESSION, END
+//    It is mandatory to prefix this command with n or z, and all commands inside IDSESSION must  be
+//    prefixed.
+//
+//    Start/end  a  clamd  session. Within a session multiple SCAN, INSTREAM, FILDES, VERSION, STATS
+//    commands can be sent on the same socket without opening new connections.  Replies  from  clamd
+//    will  be  in  the form '<id>: <response>' where <id> is the request number (in ascii, starting
+//    from 1) and <response> is the usual clamd reply.  The reply lines have same delimiter  as  the
+//    corresponding  command had.  Clamd will process the commands asynchronously, and reply as soon
+//    as it has finished processing.
+//
+//    Clamd requires clients to read all the replies it sent, before sending more commands  to  pre-vent prevent
+//    vent  send()  deadlocks. The recommended way to implement a client that uses IDSESSION is with
+//    non-blocking sockets, and  a  select()/poll()  loop:  whenever  send  would  block,  sleep  in
+//    select/poll  until either you can write more data, or read more replies.  Note that using non-blocking nonblocking
+//    blocking sockets without the select/poll loop and  alternating  recv()/send()  doesn't  comply
+//    with clamd's requirements.
+//
+//    If  clamd detects that a client has deadlocked,  it will close the connection. Note that clamd
+//    may close an IDSESSION connection too if you don't follow the protocol's requirements.
+
 
     private int timeout;
     private String host;
@@ -67,17 +90,24 @@ public class ClamScan {
             dos = new DataOutputStream(socket.getOutputStream());
             dos.write(INSTREAM);
 
+            InputStream sin = socket.getInputStream();
+
             int read;
             byte[] buffer = new byte[DEFAULT_CHUNK_SIZE];
-            while ((read = in.read(buffer))> 0){
+
+            // clamd has an explicit limit on the number of bytes which can be sent,
+            // so we need to be sure not to exceed that.  So, we check for an error condition
+            // with each write
+            while ((read = in.read(buffer))> 0 && sin.available() == 0){
                 dos.writeInt(read);
                 dos.write(buffer, 0, read);
+                dos.flush();            
             }
 
             dos.writeInt(0);
             dos.flush();
-
-            read = socket.getInputStream().read(buffer);
+ 
+            read = sin.read(buffer);
             if (read > 0) response = new String(buffer, 0, read);
 
         } finally {
